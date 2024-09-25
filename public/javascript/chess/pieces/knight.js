c57b1b181641169302bab3cf90ba6ea262befefd
wise-toddler <wiseeldrich2004@gmail.com>
2024-09-26 04:59:19 +0530

    knight handled!

--- public/javascript/chess/pieces/knight.js
+++ public/javascript/chess/pieces/knight.js
@@ -6,6 +6,26 @@ var Knight = function(config){
 
 
 Knight.prototype = new Piece({});
-Knight.prototype.move = function(newPosition){
+Knight.prototype.move = function(newPosition)
+{
+    this.position = newPosition;
+    this.render();
+}
+Knight.prototype.isValidMove = function(targetPosition)
+{
+    var currentRow = parseInt(this.position[1], 10);
+    var targetRow = parseInt(targetPosition.row, 10);
+    var currentCol = this.position[0].toUpperCase();
+    var targetCol = targetPosition.col.toUpperCase();
 
+    var rowDiff = Math.abs(currentRow - targetRow);
+    var colDiff = Math.abs(currentCol.charCodeAt(0) - targetCol.charCodeAt(0));
+    
+    if (rowDiff === 2 && colDiff === 1) {
+        return true;
+    }
+    if (rowDiff === 1 && colDiff === 2) {
+        return true;
+    }
+    return false;
 }
